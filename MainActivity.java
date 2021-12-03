ecb3f0ec5450a0aff86a83a36824aaa82d81959d
kazuki-c0p31054 <72243412+kazuki-c0p31054@users.noreply.github.com>
2021-12-03 17:17:28 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.calendar;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button FirstButton = findViewById(R.id.button1);
+        FirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button SecondButton = findViewById(R.id.button2);
+        SecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button ThirdButton = findViewById(R.id.button3);
+        ThirdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),db.class);
+                startActivity(intent);
+            }
+        });
     }
 }
